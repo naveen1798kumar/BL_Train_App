5efef85f30d9f29661c4f7f5f26d13b67b244f19
naveen-bridgelabz <naveen.kumar@bridgelabz.com>
2026-07-28 17:06:56 +0530

    UC16: Sort passenger bogie capacities using Bubble Sort

--- src/main/java/com/trainconsist/TrainConsistApp.java
+++ src/main/java/com/trainconsist/TrainConsistApp.java
@@ -608,6 +608,51 @@ public class TrainConsistApp {
         System.out.println("Application Continues...");
 
 
+        // =========================================
+// UC16 - Bubble Sort Algorithm
+// =========================================
+
+        System.out.println("\n========================================");
+        System.out.println("UC16 - SORT PASSENGER BOGIE CAPACITIES");
+        System.out.println("========================================");
+
+// Passenger bogie capacities
+        int[] capacities = {72, 24, 56, 40, 80};
+
+        System.out.println("\nBefore Sorting:");
+
+        for (int capacity : capacities) {
+            System.out.print(capacity + " ");
+        }
+
+        System.out.println();
+
+// Bubble Sort
+        for (int i = 0; i < capacities.length - 1; i++) {
+
+            for (int j = 0; j < capacities.length - 1 - i; j++) {
+
+                if (capacities[j] > capacities[j + 1]) {
+
+                    int temp = capacities[j];
+                    capacities[j] = capacities[j + 1];
+                    capacities[j + 1] = temp;
+
+                }
+
+            }
+
+        }
+
+        System.out.println("\nAfter Bubble Sort:");
+
+        for (int capacity : capacities) {
+            System.out.print(capacity + " ");
+        }
+
+        System.out.println();
+
+
         System.out.println("\nProgram Completed Successfully.");
     }
 
